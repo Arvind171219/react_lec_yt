afd44e29bf5520a613016d56f05268bc1c227e59
Arvind Kumar <arvindkr40882@gmail.com>
2023-07-10 17:25:47 +0530

    Uncontrolled Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React,{useState} from 'react'
+import './App.css';
+import React, { useRef } from 'react'
+function App() {
+  let inputRef = useRef(null)
+  let inputRef2 = useRef(null)
 
-export default function App() {
-  let [val,setVal] = useState("")
-  let [item,setItem] = useState("")
+  function submitForm(e) {
+    e.preventDefault()
+    console.warn("input field 1 value : ", inputRef.current.value)
+    console.warn("input field 2 value : ", inputRef2.current.value)
+    let input3 = document.getElementById('input3').value
+    console.warn("input field 3 value : ", input3)
+
+
+  }
   return (
-    <div className='App'>
-      <h1>Controlled Components</h1>
-      <input type="text" value={val} onChange={(e)=>setVal(e.target.value)} />
-      <input type="text" value={item} onChange={(e)=>setItem(e.target.value)} />
+    <div className="App">
+      <h1>Uncontrolled Component </h1>
+      <form onSubmit={submitForm} >
+        <input ref={inputRef} type="text" /> <br /> <br />
+        <input ref={inputRef2} type="text" /> <br /> <br />
+        <input id="input3" type="text" /> <br /> <br />
+
+        <button>Submit</button>
+      </form>
     </div>
-  )
+  );
+
 }
+export default App;
